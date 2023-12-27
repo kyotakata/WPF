4372aa3c99e593881c896865d32000add9fd7d7e
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-12-27 21:16:41 +0900

    プロジェクト ファイルを追加します。

---
+++ WPF023/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPF023
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Dto> _dtos = new ObservableCollection<Dto>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var dto1 = new Dto("Name1");
+            dto1.Dtos.Add(new Dto("Name1-1"));
+            dto1.Dtos.Add(new Dto("Name1-2"));
+            _dtos.Add(dto1);
+
+            CTreeView.ItemsSource = _dtos;
+        }
+    }
+
+    public sealed class Dto
+    {
+        public Dto(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public List<Dto> Dtos { get; set; } = new List<Dto>();
+    }
+}
